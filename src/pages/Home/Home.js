8a46d7c77b280ee3631ba09787a4e766cc3f8701
Zahra Gholipour <z.gholipour.work@gmail.com>
2023-07-05 00:55:31 +0330

    report chart added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,15 @@
 import React from "react";
-import Feature from "../../components/features/Feature";
+import Features from "./../../components/features/Feature";
+import Chart from "./../../components/Chart/Chart";
+import { xAxisData } from "../../datas";
+
+import "./Home.css";
 
 export default function Home() {
-  return <Feature />;
+  return (
+    <div className="home">
+      <Features />
+      <Chart grid title="Month Sale" data={xAxisData} dataKey="Sale" />
+    </div>
+  );
 }
